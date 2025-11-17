4e1ec1b651e0be748ebf2e3e29f5220b94392c1b
tuannguyen20198 <lntt20198@gmail.com>
2025-11-18 06:01:12 +0700

    chore: update pnpm-lock.yaml and package.json for backend dependencies, remove unused e2e tests and configurations

---
+++ backend/libs/api-client/src/utils/providers.ts
@@ -0,0 +1,21 @@
+// import { FactoryProvider } from '@nestjs/common';
+// import { AxiosInstance } from 'axios';
+
+// export function injectApiProvider<T>(
+//   ApiClass: new (
+//     configuration?: Configuration,
+//     basePath?: string,
+//     axios?: AxiosInstance,
+//   ) => T,
+// ): FactoryProvider<T> {
+//   return {
+//     provide: ApiClass,
+//     inject: [HttpService],
+//     useFactory: (httpService: HttpService) => {
+//       const config = new Configuration({
+//         basePath: process.env.SSO_SERVICE_URL || 'http://localhost:5051',
+//       });
+//       return new ApiClass(config, config.basePath, httpService.axiosRef);
+//     },
+//   };
+// }
