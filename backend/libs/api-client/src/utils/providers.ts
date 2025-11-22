6b8ac93f3a28ba3434758771aafdd36a3800757c
tuannguyen20198 <lntt20198@gmail.com>
2025-11-22 12:02:23 +0700

    chore: update package.json scripts, add path mapping in tsconfig, and refine API provider implementation

--- backend/libs/api-client/src/utils/providers.ts
+++ backend/libs/api-client/src/utils/providers.ts
@@ -1,21 +1,17 @@
-// import { FactoryProvider } from '@nestjs/common';
-// import { AxiosInstance } from 'axios';
+import { FactoryProvider } from '@nestjs/common';
+import { Configuration } from '../client/generated/configuration';
 
-// export function injectApiProvider<T>(
-//   ApiClass: new (
-//     configuration?: Configuration,
-//     basePath?: string,
-//     axios?: AxiosInstance,
-//   ) => T,
-// ): FactoryProvider<T> {
-//   return {
-//     provide: ApiClass,
-//     inject: [HttpService],
-//     useFactory: (httpService: HttpService) => {
-//       const config = new Configuration({
-//         basePath: process.env.SSO_SERVICE_URL || 'http://localhost:5051',
-//       });
-//       return new ApiClass(config, config.basePath, httpService.axiosRef);
-//     },
-//   };
-// }
+export function injectApiProvider<T>(
+    ApiClass: new (configuration?: Configuration, basePath?: string) => T,
+): FactoryProvider<T> {
+  const basePath = process.env.API_SERVICE_URL || 'http://localhost:5050';
+  return {
+    provide: ApiClass,
+    useFactory: () => {
+      const config = new Configuration({
+        basePath: process.env.API_SERVICE_URL || 'http://localhost:5050',
+      });
+      return new ApiClass(config, config.basePath);
+    },
+  };
+}
